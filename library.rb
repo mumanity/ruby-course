e474d65c3f686bf6d220c464bfa4239a11dcff24
Emily Schomer <minischomer@gmail.com>
2014-04-22 16:19:47 -0500

    enable check out by id

--- library.rb
+++ library.rb
@@ -1,7 +1,7 @@
 
 class Book
-  attr_reader :author, :title, :status
-  attr_accessor :id
+  attr_reader :author, :title
+  attr_accessor :id, :status
 
   def initialize(title, author)
     @title = title
@@ -34,6 +34,7 @@ end
 
 class Library
   attr_reader :title, :author, :books
+  attr_accessor :borrower
 
   def initialize(name)
     @books = []
@@ -45,7 +46,18 @@ class Library
   end
 
   def check_out_book(book_id, borrower)
-    @status = 'checked_out'
+    # @status = 'checked_out'
+    @books.each do |book|
+      if book.id == book_id
+        if book.status == "available"
+          book.status = "checked_out"
+          return book
+          #book.borrower = borrower
+        else
+          false
+        end
+      end
+    end
   end
 
   def check_in_book(book)
